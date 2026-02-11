59f266979246b918aaaea4a1a27e4116caf1268a
J03L-arch <joelmwega3041@gmail.com>
2026-02-11 10:42:50 +0300

    Program: To add, subtract, multiply, divide numbers

--- python basics/Data types.py
+++ python basics/Data types.py
@@ -2,6 +2,12 @@
 #Date:11,02,2026
 #Program:To add two numbers
 
+#program:To get use input
+name=input("What is your name?")
+print (name)
+
+
+
 first_number=67
 second_number=69
 sum_numbers= first_number + second_number
---
+++ python basics/numbers.py
@@ -0,0 +1,15 @@
+#Name:Joel Mwega
+#Date:11,02,2026
+#Program:To add, subtract, multiply, divide numbers
+
+first_number=int(input("Enter th first number"))
+second_number=int(input("Enter the second number"))
+sum_numbers= first_number+ second_number
+difference_numbers= first_number - second_number
+product_numbers=first_number * second_number
+quotient_numbers=first_number / second_number
+
+print(f"The sum of numbers is {sum_numbers}")
+print(f"The difference of numbers is {difference_numbers}")
+print(f"The product of numbers is {product_numbers}")
+print(f"The quotient of numbers is {quotient_numbers}")

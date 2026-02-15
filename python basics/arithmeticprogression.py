0b6befd822f686931bb2b38661081af82fccf389
J03L-arch <joelmwega3041@gmail.com>
2026-02-16 00:00:33 +0300

    Loops, AP and Gp

--- python basics/arithmeticprogression.py
+++ python basics/arithmeticprogression.py
@@ -7,7 +7,10 @@
 a= int(input("Enter the first term:"))
 n= int(input("Enter the the number of terms:"))
 d= int(input("Enter the common difference:"))
+r=int(input("Enter the common ratio:"))
+
 nth_term= int (a+ (n -1)*d)
 print(f"The nth term is{nth_term}")
 Sn= int(n/2 *((2*a + (n-1)*d)))
 print (f"Sum of the terms is: {Sn}")
+Sn=int((a*((r**n)-1))/(r-1))

0f0c63e414e7f0db2c121d0cc0f678f1023814d4
J03L-arch <joelmwega3041@gmail.com>
2026-02-16 13:41:45 +0300

    Assignment 5

--- python basics/Assignment5.py
+++ python basics/Assignment5.py
@@ -7,6 +7,14 @@ if salary< 50000:
     tax= (2.5 * salary)/100
     net_salary= salary- tax
 
+if 50000< salary <=100000:
+    tax= (4.5* salary)/100
+    net_salary= salary- tax
+
+if salary>100000:
+    tax= (7.5* salary)/100
+    net_salary= salary- tax
+
 print(f"Gross salary={salary}")    
 print(f"tax={tax}")
 print(f"net salary={net_salary}")

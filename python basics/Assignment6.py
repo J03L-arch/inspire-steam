67c0c340333be8fdb9e9c2abcede16abb15dafab
J03L-arch <joelmwega3041@gmail.com>
2026-02-17 23:32:25 +0300

    Assignment 6

--- python basics/Assignment6.py
+++ python basics/Assignment6.py
@@ -1,4 +1,24 @@
 #Name:Joel Mwega
 #Date:16,02,2026
 #Program: 1.Write a python program to display a triangle and diamond using *
-          
+
+print("TRIANGLE")
+print("     *     ")
+print("    * *    ")
+print("   *   *   ")
+print("  *     *  ")
+print(" *       * ")
+print("* * * * * *")
+
+print("DIAMOND")
+print("     *     ")
+print("    * *    ")
+print("   *   *   ")
+print("  *     *  ")
+print(" *       * ")
+print("*         *")
+print(" *       * ")
+print("  *     *  ")
+print("   *   *   ")
+print("    * *    ")
+print("     *     ")
